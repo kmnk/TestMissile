8ea0c266fb465cf514a9c82728a6e1d9d0284b25
KMNK <kmnknmk+com-github@gmail.com>
2018-05-27 15:49:07 +0900

    Add test missile

---
+++ Assets/TestMissile/Scripts/Missile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+    [SerializeField]
+    GameObject target;
+
+    float ratio = 3000f;
+
+    // カプセルの向きを補正
+    Quaternion rotOffset = Quaternion.AngleAxis(90.0f, Vector3.right);
+
+    Vector3 _velocity;
+    Vector3 _position;
+
+    void Start()
+    {
+        _position = transform.position;
+    }
+
+    void Update()
+    {
+        var acceleration = transform.up * 200f;
+
+        // velocity の最大値を決めておかないとだんだん振幅が大きくなってしまう？
+        _velocity += acceleration * Time.deltaTime;
+        _position += _velocity * Time.deltaTime;
+        transform.position = _position;
+    }
+
+    void FixedUpdate()
+    {
+        var diff = target.transform.position - transform.position;
+
+        var targetRot = Quaternion.LookRotation(diff) * rotOffset;
+
+        var q = targetRot * Quaternion.Inverse(transform.rotation);
+
+        var torque = new Vector3(q.x, q.y, q.z) * ratio;
+
+        GetComponent<Rigidbody>().AddTorque(torque);
+    }
+
+}
